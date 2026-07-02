d02c11cada728980edfac849010671f3c09eff0e
srreuk <sjck9hy6ps@privaterelay.appleid.com>
2026-07-03 00:29:50 +0900

    Refactor API handler to use fetch instead of https

--- api/house.js
+++ api/house.js
@@ -1,40 +1,33 @@
-const https = require('https');
-
-export default function handler(req, res) {
+export default async function handler(req, res) {
     const { endpoint, lawdCd, dealYmd } = req.query;
     const serviceKey = 'ca4e98f4254eccbbabfbb3f9f972e17eba48507e804a9ac2bc97260423a090d6';
 
     let safeYmd = dealYmd;
     if (parseInt(dealYmd) > 202412) safeYmd = '202412';
 
-    const path = endpoint === 'aptRent' 
-        ? `/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptRent?serviceKey=${serviceKey}&LAWD_CD=${lawdCd}&DEAL_YMD=${safeYmd}`
-        : `/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?serviceKey=${serviceKey}&LAWD_CD=${lawdCd}&DEAL_YMD=${safeYmd}`;
+    // 어제 성공의 핵심: URL 전체를 명확하게 구성하여 fetch에 전달
+    const baseUrl = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc';
+    const apiPath = endpoint === 'aptRent' 
+        ? `/getRTMSDataSvcAptRent`
+        : `/getRTMSDataSvcAptTradeDev`;
+    
+    const fullUrl = `${baseUrl}${apiPath}?serviceKey=${serviceKey}&LAWD_CD=${lawdCd}&DEAL_YMD=${safeYmd}`;
 
-    const options = {
-        hostname: 'openapi.molit.go.kr',
-        port: 443,
-        path: path,
-        method: 'GET',
-        rejectUnauthorized: false  // 인증서 문제 방지 (테스트용)
-    };
-
-    console.log('Requesting:', `https://${options.hostname}${options.path}`); // 디버깅 로그
-
-    const request = https.request(options, (response) => {
-        let data = '';
-        response.on('data', (chunk) => { data += chunk; });
-        response.on('end', () => {
-            res.setHeader('Content-Type', 'application/xml; charset=utf-8');
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.status(200).send(data);
+    try {
+        const response = await fetch(fullUrl, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/xml',
+            }
         });
-    });
 
-    request.on('error', (e) => {
-        console.error('House API Error:', e.message);
+        const data = await response.text();
+
+        res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.status(200).send(data);
+    } catch (error) {
+        console.error('House API Error:', error);
         res.status(200).send('<?xml version="1.0" encoding="UTF-8"?><response><body><items></items></body></response>');
-    });
-
-    request.end();
+    }
 }
