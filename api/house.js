e76d0bdd5a2620a9bb9082e5e4c3dc650c88f5bd
srreuk <sjck9hy6ps@privaterelay.appleid.com>
2026-07-03 00:16:34 +0900

    Refactor house API handler to use HTTPS requests

--- api/house.js
+++ api/house.js
@@ -1,24 +1,40 @@
-export default async function handler(req, res) {
+const https = require('https');
+
+export default function handler(req, res) {
     const { endpoint, lawdCd, dealYmd } = req.query;
     const serviceKey = 'ca4e98f4254eccbbabfbb3f9f972e17eba48507e804a9ac2bc97260423a090d6';
 
     let safeYmd = dealYmd;
     if (parseInt(dealYmd) > 202412) safeYmd = '202412';
 
-    const baseUrl = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc';
     const path = endpoint === 'aptRent' 
-        ? `/getRTMSDataSvcAptRent?serviceKey=${serviceKey}&LAWD_CD=${lawdCd}&DEAL_YMD=${safeYmd}`
-        : `/getRTMSDataSvcAptTradeDev?serviceKey=${serviceKey}&LAWD_CD=${lawdCd}&DEAL_YMD=${safeYmd}`;
+        ? `/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptRent?serviceKey=${serviceKey}&LAWD_CD=${lawdCd}&DEAL_YMD=${safeYmd}`
+        : `/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?serviceKey=${serviceKey}&LAWD_CD=${lawdCd}&DEAL_YMD=${safeYmd}`;
 
-    try {
-        const response = await fetch(baseUrl + path);
-        const data = await response.text();
-        
-        res.setHeader('Content-Type', 'application/xml; charset=utf-8');
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.status(200).send(data);
-    } catch (error) {
-        console.error('House API Error:', error);
+    const options = {
+        hostname: 'openapi.molit.go.kr',
+        port: 443,
+        path: path,
+        method: 'GET',
+        rejectUnauthorized: false  // 인증서 문제 방지 (테스트용)
+    };
+
+    console.log('Requesting:', `https://${options.hostname}${options.path}`); // 디버깅 로그
+
+    const request = https.request(options, (response) => {
+        let data = '';
+        response.on('data', (chunk) => { data += chunk; });
+        response.on('end', () => {
+            res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.status(200).send(data);
+        });
+    });
+
+    request.on('error', (e) => {
+        console.error('House API Error:', e.message);
         res.status(200).send('<?xml version="1.0" encoding="UTF-8"?><response><body><items></items></body></response>');
-    }
+    });
+
+    request.end();
 }
